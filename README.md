73bce3c8ab8c0a187f82b872afd6802e272de6a8
David Desmaisons <desmaisons_david@hotmail.com>
2020-01-17 14:11:35 -0300

    bootstraping

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-Minesweeper
+# MultiFrameworkApp
+
+Template to bootstrap mvi application.
+
+## Installation
+
+Using degit:
+
+
+1. install degit
+
+```bash
+npm install -g degit
+```
+
+2. Clone the template
+
+```bash
+degit JS-Dev-Lab/MultiFrameworkApp my-new-project
+```
+
+3. Start working
+
+```bash
+cd my-new-project
+npm install
+npm run serve
+```
---
+++ src/frameworks/react/App.js
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { ...props.state };
+  }
+
+  render() {
+    const { state } = this;
+    const { commands: { add, setName } } = this.props;
+    const onChange = event => {
+      setName(event.target.value);
+    };
+    return (
+      <div className="App">
+        <h1>Hello {state.name}</h1>
+        <input value={state.name} onChange={onChange}></input>
+        <p>{state.name.length}</p>
+        <p>{state.count}</p>
+        <button onClick={add}>My button</button>
+      </div>
+    );
+  }
+}
+
+export default App;
