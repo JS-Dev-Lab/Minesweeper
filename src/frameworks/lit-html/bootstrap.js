123d49f90ce17cab094d19f25d037b06e2cc522f
David <desmaisons_david@hotmail.com>
2020-01-27 23:10:53 -0300

    Add lit-html

--- src/frameworks/lit-html/bootstrap.js
+++ src/frameworks/lit-html/bootstrap.js
@@ -4,4 +4,47 @@ import { template } from "./template";
 const element = document.getElementById("app");
 const createView = viewCreatorFactory(element, template);
 
+const style = document.createElement("style");
+style.type = "text/css";
+style.appendChild(
+  document.createTextNode(`
+.cell {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-weight: bold;
+  font-size: 20px;
+}
+.cell.unseen {
+  background: lightgrey;
+}
+.cell.mine.seen {
+  background: red;
+  color: black;
+}
+.cell.number-1 {
+  color: blue;
+}
+.cell.number-2 {
+  color: green;
+}
+.cell.number-3 {
+  color: red;
+}
+.cell.number-4 {
+  color: orange;
+}
+.cell.number-5 {
+  color: yellow;
+}
+.cell.number-6 {
+  color: black;
+}
+.cell.number-7 {
+  color: purple;
+}`)
+);
+const head = document.getElementsByTagName("head")[0];
+head.appendChild(style);
+
 export { createView };
--- src/frameworks/lit-html/template.js
+++ src/frameworks/lit-html/template.js
@@ -1,11 +1,43 @@
-import { html } from 'lit-html';
+import { html, nothing } from 'lit-html';
+import { repeat } from 'lit-html/directives/repeat';
 
-function template({ state: { array, name, count }, commands }) {
-  return html`<h1>Hello ${name}</h1>
- <input value=${name} @input=${(e) => commands.setName(e.target.value)}></input>
- <p>${name.length}</p>
- <p>${count}</p><button @click=${() => commands.add()}>My button</button>
- <ul>${array.map(value => html`<li>${value}</li>`)}</ul>`;
+
+function formatter({ seen, mine, neighborMines }) {
+  if (!seen) {
+    return "";
+  }
+  if (mine) {
+    return "*";
+  }
+  return neighborMines === 0 ? "" : neighborMines;
+}
+
+function getCellClass({x, y, cell: {neighborMines, seen, mine}}){
+  return `cell position-${x}-${y} number-${neighborMines} ${seen ? "seen" : "unseen"} ${mine ? "mine" : ""}`;
+}
+
+function template({
+  state: { array, status, height, width, timeSpend },
+  commands
+}) {
+  return html`<style>
+            .wrapper {
+              display: grid;
+              grid-template-columns: repeat(${height}, 30px);
+              grid-template-rows: repeat(${width}, 30px);
+              grid-gap: 2px;
+            }
+          </style>
+          <div class="wrapper">
+          ${repeat(
+            array.flatMap((row, x) => row.map((cell, y) => ({cell, x, y}))),
+            ({x, y}) => `${x}-${y}`,
+            ({x, y, cell}) => html`<div class=${getCellClass({x, y, cell})} @click=${() => commands.guess({x,y})}>${formatter(cell)}</div>`
+          )}
+          </div>
+          ${status !== "running" ? html`<p>you ${status}!!!!</p>` : nothing}
+          <p>${timeSpend}</p>
+          <button @click=${() => commands.reRun()}>Restart</button>`;
 }
 
 export { template };
